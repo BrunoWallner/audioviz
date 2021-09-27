4d7e0aa3bb0432f8be8ddef5f10fc90a2628ebd8
Luca <b.lucab1211@gmail.com>
2021-09-27 20:48:16 +0200

    updated version

--- src/config.rs
+++ src/config.rs
@@ -9,21 +9,21 @@ pub struct Config {
     pub frequency_scale_amount: usize,
     pub density_reduction: usize,
     pub max_frequency: usize,
-    pub volume_amplitude: f32,
+    pub volume: f32,
 }
 impl Default for Config {
     fn default() -> Self {
         Config { 
-            buffering: 7,
-            smoothing_size: 35,
+            buffering: 5,
+            smoothing_size: 5,
             smoothing_amount: 5,
             resolution: 3000,
             refresh_rate: 60,
             frequency_scale_range: [0, 3500],
             frequency_scale_amount: 3,
             density_reduction: 10,
-            max_frequency: 17500,
-            volume_amplitude: 1.0,
+            max_frequency: 20_000,
+            volume: 1.0,
         }
     }
 }
